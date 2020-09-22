6019d2aa66be2dd7c9a5be1422f0c0e551d0e82d
jhonathanmusa <jonathansm891@gmail.com>
2020-09-21 20:23:15 -0500

    Second commit

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get("/", (req, res) => {
   res.render("pages/index.ejs");
 });
 
+router.get("/users", (req, res) => {
+  res.render("pages/users.ejs");
+});
+
+router.get("/about", (req, res) => {
+  res.render("pages/about.ejs");
+});
+
 module.exports = router;
