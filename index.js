bbae936e0bcbd829e2891dff7631657da08e2704
jhonathanmusa <jonathansm891@gmail.com>
2020-09-30 18:21:47 -0500

    Updating the user view

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ const url = "mongodb://localhost:27017";
 Mongodb.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((user) => {
     console.log("Database Connected");
-    const db = user.db("prueba");
-    const companyCollection = db.collection("personas");
+    const db = user.db("company");
+    const companyCollection = db.collection("employ");
 
-    app.use(bodyParser.urlencoded({ extended: false }));
     app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({ extended: false }));
 
     app.use(require("./routes/index"));
 
