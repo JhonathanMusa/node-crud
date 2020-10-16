d6c9cc5657ad51571549631022855de14f74fe03
jhonathanmusa <jonathansm891@gmail.com>
2020-10-15 22:36:21 -0500

    updating some parts of the index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const bodyParser = require("body-parser");
 const Mongodb = require("mongodb");
 const url = "mongodb://localhost:27017";
 const db_name = "company";
@@ -12,7 +11,8 @@ Mongodb.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     const companyCollection = db.collection("employ");
 
     app.set("view engine", "ejs");
-    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }));
 
     app.use(require("./routes/index"));
 
@@ -29,7 +29,7 @@ Mongodb.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
       const query = req.body;
       companyCollection.find(query).toArray((err, result) => {
         if (err) throw err;
-        console.log(result);
+        res.status(200).json(query);
         res.redirect("/view");
       });
     });
@@ -43,8 +43,7 @@ Mongodb.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
         .catch((error) => console.error(error));
     });
 
-    app.listen(8080, () => {
-      console.log("Server running on port 8080");
-    });
+    app.listen(8080);
+    console.log("Server running on port 8080");
   })
   .catch((error) => console.error(error));
